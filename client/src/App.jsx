00ce2b3fc8585128b0af62b559ccf57b7ae717a0
Henry James Carlos <henryjamescarlos@gmail.com>
2024-11-02 17:20:24 +0800

    refactor: reorganize files to optimize folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react"
-import Navbar from "./components/Navbar/Navbar"
+import Landing from "./pages/Landing/Landing"
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Landing/>
     </div>
   )
 }
---
+++ client/src/pages/Landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Preview from '../../components/Preview/Preview'
+
+function Landing() {
+  return (
+    <div>
+        <Navbar/>
+        <Preview/>
+    </div>
+  )
+}
+
+export default Landing
