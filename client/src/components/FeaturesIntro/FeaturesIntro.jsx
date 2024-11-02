98ee946130588e0590c59bd21cd899a393a0d7ce
Henry James Carlos <henryjamescarlos@gmail.com>
2024-11-03 04:44:31 +0800

    feat(landing): add section for FeaturesIntro.jsx

--- client/src/components/FeaturesIntro/FeaturesIntro.jsx
+++ client/src/components/FeaturesIntro/FeaturesIntro.jsx
@@ -7,7 +7,7 @@ import points from '../../assets/points-icon.png'
 
 const FeaturesIntro = () => {
   return (
-    <div className='intro-container'>
+    <div id="features" className='intro-container'>
         <div className="intro-header">
             <div className="intro-label">Features</div>
             <h1>Unlock Smarter Learning and Seamless Productivity</h1>
@@ -15,21 +15,24 @@ const FeaturesIntro = () => {
         <div className="intro-card-container">
             <div className="features-desc">
                 <div className="flashcards-desc">
-                    <img src={cards} alt="Cards" />
+                    <img src={cards} alt="Cards" className='features-icon' />
                     <p>Create custom flashcards and track difficulty ratings for targeted study sessions.</p>
                 </div>
                 <div className="pomodoro-desc">
-                    <img src={timer} alt="Timer" />
+                    <img src={timer} alt="Timer" className='features-icon' />
                     <p>Utilize task tracking with Pomodoro timers for deep work and time management.</p>
                 </div>
                 <div className="gamify-desc">
-                    <img src={points} alt="Points" />
+                    <img src={points} alt="Points"  className='features-icon'/>
                     <p>Earn points, level up, and see your achievements through interactive elements and streaks.</p>
                 </div>
             </div>
-            <div className="dashboard-bg">
-                <img src={dashboard} alt="Dashboard" className='dashboard-pic'/>
+            <div className="dashboard-container">
+                <div className="dashboard-bg">
+                    <img src={dashboard} alt="Dashboard" className='dashboard-pic'/>
+                </div>
             </div>
+            
         </div>
     </div>
   )
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,13 +3,27 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 
 const Navbar = () => {
+  const scrollToFeatures = () => {
+    const element = document.getElementById("features");
+    if (element) {
+        element.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+
+  const scrolltoTestimonials = () => {
+    const element = document.getElementById("testimonials");
+    if (element) {
+        element.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+
   return (
     <nav>
         <img src={logo} alt="Logo" className='logo' />
         <ul className='navbar-ul'>
             <li><span className="circle-1"></span>About Us</li>
-            <li><span className="circle-2"></span>Features</li>
-            <li><span className="circle-3"></span>Testimonials</li>
+            <li onClick={scrollToFeatures}><span className="circle-2"></span>Features</li>
+            <li onClick={scrolltoTestimonials}><span className="circle-3"></span>Testimonials</li>
         </ul>
         <div className='nav-btn'>
             <button className='login-btn'>Log In</button>
