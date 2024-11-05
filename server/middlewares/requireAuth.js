16601b3b7177d31c1ecf87069837bd67c6ec29b7
jellyM0on <ylana.ong@gmail.com>
2024-11-05 09:07:59 +0800

    feat(api/deck): add auth middleware to deck routes

---
+++ server/middlewares/requireAuth.js
@@ -0,0 +1,7 @@
+export default function isAuthenticated(req, res, next){
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.status(401).json({message: "Unauthorized"})
+}
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from "express"; 
 import authRouter from "./auth.js"; 
+import requireAuth from "../middlewares/requireAuth.js"
 import deckRouter from "./decks.js"
 
 const router = Router(); 
 
 router.use(authRouter); 
-router.use(deckRouter); 
+
+router.use(requireAuth); 
+
+router.use(deckRouter);
 
 export default router; 
