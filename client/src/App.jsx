afcf5091d7ef3140f22a7364e8e2b07b96f570b9
Fervicmar <fervicmardlagman@gmail.com>
2024-12-19 19:56:01 +0800

    feat(signup): update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import NewPassword from "./pages/NewPassword/NewPassword";
+import Interest from "./pages/Interest/Interest";
 import "./App.css";
 
 function App() {
@@ -10,8 +11,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/new-password" element={<NewPassword />} />
+        <Route path="/interest" element={<Interest />} />
       </Routes>
     </Router>
   );
